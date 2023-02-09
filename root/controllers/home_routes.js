c400770342c3fdc79cdedea204915c96f2937fa1
maxwell walters <118125584+4therealm@users.noreply.github.com>
2023-02-09 17:02:02 -0600

    single posts now show the post, comments, and the writer of the comment

--- root/controllers/home_routes.js
+++ root/controllers/home_routes.js
@@ -1,19 +1,59 @@
 const router = require("express").Router();
+const {Error}=require("sequelize")
 const sequelize = require("../config/connection");
 const Post = require("../models/Post");
-
+const User = require("../models/User");
+const PostComment = require("../models/PostComment");
+const Comment = require("../models/Comments");
+const withAuth = require('../utils/auth')
 router.get("/", async (req, res) => {
   const postData = await Post.findAll().catch((err) => {
     res.json(err);
   });
-
   const posts = postData.map((post) => post.get({ plain: true }));
-
   res.render("home", {
     posts,
     loggedIn: req.session.loggedIn,
   });
 });
+
+
+
+// router.get("/post/:id", async (req, res) => {
+
+//   const postData = await Post.findByPk(req.params.id, {
+    
+//   })
+//     const thePost = postData.get({plain: true});
+//   res.render('post',{thePost})
+
+
+//   //  try {
+// //   const postData = await Post.findByPk(req.params.id, {
+// //     include: [
+// //       {
+// //         model: Comment,
+// //         as: 'comments',
+// //         where: {
+// //           post_id: postData.id
+// //         }
+// //       }
+// //     ]
+// //   })
+// //   if(!postData){
+// //     return res.status(404).json({message: 'post not found'})
+// //   }
+// //   const thePost = postData.get({plain: true});
+// //   res.render('post',{ thePost, loggedIn: req.session} )
+// //  } catch (error) {
+// //   console.log(err);
+// //   res.status(500).json(err);
+// //  }
+// //  res.render('post', {message: "it works"})
+ 
+// } )
+
+
 
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
@@ -22,6 +62,45 @@ router.get("/login", (req, res) => {
   }
   res.render("login");
 });
+// router.get("/post/:id", async (req, res) => {
+//   const postData = await Post.findByPk(req.params.id, {
+//     include: [
+//       {
+//         model: Comment,
+//         as: 'comments',
+//         where: {
+//           post_id: req.params.id
+//         }
+//       },
+//     ]
+//   })
+//   const thePost = postData.get({plain: true});
+//   res.render('post', { post: thePost });
+// });
+router.get("/post/:id", async (req, res) => {
+  const postData = await Post.findByPk(req.params.id, {
+    include: [
+      {
+               model: Comment,
+               as: 'comments',
+               where: {
+                 post_id: req.params.id
+               },
+               include: [
+                 {
+                   model: User,
+                   as: 'user'
+                 }
+               ]
+  },
+]
+  })
+    const thePost = postData.get({plain: true});
+  res.render('post',{post: thePost})
+});
+
+
+
 router.get("/logout", (req, res) => {
   if (!req.session.loggedIn) {
     res.redirect("/");
